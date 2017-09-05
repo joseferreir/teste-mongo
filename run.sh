94f48c0891f47d4f628f5fb00859780b82464cea
josé <joseferreiravieira123@gmail.com>
2017-09-04 23:15:35 -0300

    coreções de dependencias

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 mvn clean install 
 
+docker rmi core
+docker rmi webapp
+
 docker build -t core ./EMS-core/
 docker build -t webapp ./EMS-web/
 
-docker run -p 27017:27017 -d --name banco mongo   
+docker run -p 27017:27017 -d --name banco mongo  
+ 
 docker run -p 8080:8080 -d --name appcore --link banco:host-banco core -d
+
 docker run -p 8081:8080 -d --name appweb --link appcore:host-core webapp -d
